ac57625c5af74e653410921f05c93f7220e8ea0d
DK Kang <devkade12@gmail.com>
2025-11-30 17:47:50 +0900

    add pybullet_exam

---
+++ src/pybullet_homework/case_pick_and_place_blind.py
@@ -0,0 +1,67 @@
+import pybullet as p
+import pybullet_data
+import time
+import numpy as np
+
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.8)
+
+p.loadURDF("plane.urdf")
+table = p.loadURDF("table/table.urdf", [0.5, 0, 0], useFixedBase=True)
+robot = p.loadURDF("franka_panda/panda.urdf", basePosition=[0.0, 0, 0.66], useFixedBase=True)
+
+box_6 = p.loadURDF("models/box6.xacro", basePosition=[0.3, -0.1, 0.66])
+box_5 = p.loadURDF("models/box5.xacro", basePosition=[0.3, 0.0, 0.66])
+box_4 = p.loadURDF("models/box4.xacro", basePosition=[0.3, 0.1, 0.66])
+
+box_3 = p.loadURDF("models/box3.xacro", basePosition=[0.4, -0.1, 0.66])
+cylinder_0 = p.loadURDF("models/cylinder1.xacro", basePosition=[0.4, 0.0, 0.66])
+box_2 = p.loadURDF("models/box2.xacro", basePosition=[0.4, 0.1, 0.66])
+
+box_0 = p.loadURDF("models/box.xacro", basePosition=[0.5, 0.0, 0.66])
+triangle = p.loadURDF("models/triangle.xacro", basePosition=[0.5, 0.1, 0.66])
+
+case_collision = p.createCollisionShape(
+    shapeType=p.GEOM_MESH,
+    fileName="models/case.obj", 
+    flags=p.GEOM_FORCE_CONCAVE_TRIMESH,
+    meshScale=[1, 1, 1]
+)
+
+case_visual = p.createVisualShape(
+    shapeType=p.GEOM_MESH,
+    fileName="models/case.obj",
+    meshScale=[1, 1, 1]
+)
+
+case = p.createMultiBody(
+    baseMass=0.05,
+    baseCollisionShapeIndex=case_collision,
+    baseVisualShapeIndex=case_visual,
+    basePosition=[0.0, 0.3, 0.64],
+    baseOrientation=p.getQuaternionFromEuler([0, 0, 3.14159])
+)
+
+
+p.changeVisualShape(box_6, -1, rgbaColor=[1, 0, 1, 1])  # box
+p.changeVisualShape(box_5, -1, rgbaColor=[1, 0, 0, 1])  # box2
+p.changeVisualShape(box_4, -1, rgbaColor=[1, 0, 0, 1])  # box3
+
+p.changeVisualShape(box_3, -1, rgbaColor=[1, 0, 0, 1])  # box4
+p.changeVisualShape(cylinder_0, -1, rgbaColor=[0, 0, 1, 1])  # box5
+p.changeVisualShape(box_2, -1, rgbaColor=[1, 0, 0, 1])  # box6
+
+p.changeVisualShape(box_0, -1, rgbaColor=[1, 0, 0, 1])  # box5
+p.changeVisualShape(triangle, -1, rgbaColor=[1, 0, 1, 1])  # box6
+
+p.resetDebugVisualizerCamera(cameraDistance=1.5, cameraYaw=45, cameraPitch=-30, cameraTargetPosition=[0.3, 0, 0.5])
+
+## 기본적인 Scene 코드 이후 부분을 제공된 Task를 수행하도록 구현하세요 ##
+
+for _ in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+
+p.disconnect()
+
